e3e49645650ec1eda257a744b7d8f7c6143e3733
Emmanuel <tuemail@ejemplo.com>
2026-01-16 23:37:09 -0300

    Primera versión: App de gastos con Firebase

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   base: "/Control-de-Gastos/",
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 })
